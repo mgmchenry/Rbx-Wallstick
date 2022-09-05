4215678b2ced5abbbd78b1596db78b667adc2779
mgmchenry <michael.genesis@gmail.com>
2022-09-05 18:58:21 -0400

    Remove CameraInjector dependency
    Moved camera update UpVector fixes to BaseCamera to avoid need for CameraInjector

--- src/ServerScriptService/Wallstick/Client/PlayerScriptsLoader/init.client.lua
+++ src/ServerScriptService/Wallstick/Client/PlayerScriptsLoader/init.client.lua
@@ -10,9 +10,8 @@ local MAX_Y = math.rad(80)
 local ZERO3 = Vector3.new(0, 0, 0)
 
 local PlayerModule = script.Parent:WaitForChild("PlayerModule")
-local CameraInjector = script:WaitForChild("CameraInjector")
-
-require(CameraInjector)
+--local CameraInjector = script:WaitForChild("CameraInjector")
+--require(CameraInjector)
 
 -- Control Modifications
 
@@ -36,12 +35,24 @@ end
 
 -- Camera Modifications
 
+local function getStaticCameraModules()
+	local mods = {}
+	local playerModule = game:GetService("ReplicatedStorage").PlayerModule
+	mods.BaseCamera = require(playerModule.CameraModule.BaseCamera)
+	mods.CameraModule = require(playerModule.CameraModule)
+	return mods
+end
+
+type BaseCamera = typeof(getStaticCameraModules().BaseCamera)
+type CameraModule = typeof(getStaticCameraModules().CameraModule)
 local CameraModule = PlayerModule:WaitForChild("CameraModule")
 
 local UserSettings = require(script:WaitForChild("FakeUserSettings"))
-local UserGameSettings = UserSettings():GetService("UserGameSettings")
+local UserGameSettings = 
+	--game:GetService("UserGameSettings") 
+	UserSettings():GetService("UserGameSettings")
 
-local FFlagUserFlagEnableNewVRSystem = UserSettings():SafeIsUserFeatureEnabled("UserFlagEnableNewVRSystem")
+--local FFlagUserFlagEnableNewVRSystem = UserSettings():SafeIsUserFeatureEnabled("UserFlagEnableNewVRSystem")
 
 -- Camera variables
 
@@ -64,6 +75,53 @@ function Utils.GetAngleBetweenXZVectors(v1, v2)
 	return math.atan2(v2.X*v1.Z-v2.Z*v1.X, v2.X*v1.X+v2.Z*v1.Z)
 end
 
+local cameraControllers = {}
+local BaseCamera:BaseCamera = require(CameraModule:WaitForChild("BaseCamera"))
+local BaseCamera_new = BaseCamera.new
+function BaseCamera:UpdateWithUpVector(dt,newCameraCFrame, newCameraFocus)
+	return newCameraCFrame, newCameraFocus
+end
+function BaseCamera.new()
+	local env = getfenv(2)
+	local camScript:ModuleScript = env.script
+
+	local newSelfTable:BaseCamera = BaseCamera_new()
+	-- camera controller implementation (such as ClassicCamera) has called BaseCamera.new
+	-- newSelfTable will be returned to actual camera controller implementation to be initialized
+	-- actual controller module table will be set as the metatable of newSelfTable
+	-- we can wrap any calls to the actual controller methods before we return newSelfTable:
+	if typeof(camScript)=="Instance" and camScript:IsA("ModuleScript") then -- sanity check
+		print("BaseCamera.new called for camera controller: ", camScript:GetFullName())
+		local controllerInfo = {
+			Name = camScript.Name,
+			Script = camScript,
+			ControllerSelf = newSelfTable,
+		}
+		cameraControllers[controllerInfo.Name] = controllerInfo
+		function newSelfTable:Update(dt)
+			local cameraControllerMeta:BaseCamera = getmetatable(self) -- this is the underlying camera controller __index and metatable
+			local newCameraCFrame, newCameraFocus 
+				=  cameraControllerMeta.Update(self, dt) -- get cframe and focus from underlying camera controller update
+			return self:UpdateWithUpVector(dt, newCameraCFrame, newCameraFocus)
+		end
+		-- sanity check:
+		function newSelfTable:Enable(enable: boolean)
+			if self~=newSelfTable then
+				print("Something is wrong. expected self==baseTable. There will be problems")
+			end
+			local cameraControllerMeta:BaseCamera = getmetatable(self)
+			print("Enabling camera controller:", controllerInfo.Name)
+			cameraControllerMeta.Enable(self, enable)
+		end
+	else
+		print("Can't find script calling BaseCamera.new", env)
+	end
+	return newSelfTable
+end
+
+local Camera = require(CameraModule)
+print(cameraControllers)
+
 -- Popper Camera
 
 local Poppercam = require(CameraModule:WaitForChild("Poppercam"))
@@ -78,7 +136,7 @@ end
 
 -- Base Camera
 
-local BaseCamera = require(CameraModule:WaitForChild("BaseCamera"))
+--local BaseCamera = require(CameraModule:WaitForChild("BaseCamera"))
 
 function BaseCamera:CalculateNewLookCFrameFromArg(suppliedLookVector, rotateInput)
 	local currLookVector = suppliedLookVector or self:GetCameraLookVector()
@@ -162,7 +220,7 @@ local function calculateSpinCFrame(self)
 	prevSpinCFrame = spinPart.CFrame
 end
 
-local Camera = require(CameraModule)
+--local Camera = require(CameraModule)
 local CameraInput = require(CameraModule:WaitForChild("CameraInput"))
 
 function Camera:GetUpVector(oldUpVector)
@@ -181,7 +239,25 @@ function Camera:GetTransitionRate()
 	return transitionRate
 end
 
-function Camera:Update(dt)
+BaseCamera.UpdateWithUpVector = function(self, dt, newCameraCFrame, newCameraFocus)
+	newCameraFocus = CFrame.new(newCameraFocus.p) -- vehicle camera fix
+
+	calculateUpCFrame(Camera, dt)
+	calculateSpinCFrame(Camera)
+
+	local lockOffset = Vector3.new(0, 0, 0)
+	if Camera.activeMouseLockController and Camera.activeMouseLockController:GetIsMouseLocked() then
+		lockOffset = Camera.activeMouseLockController:GetMouseLockOffset()
+	end
+
+	local offset = newCameraFocus:ToObjectSpace(newCameraCFrame)
+	local camRotation = upCFrame * twistCFrame * offset
+	newCameraFocus = newCameraFocus - newCameraCFrame:VectorToWorldSpace(lockOffset) + camRotation:VectorToWorldSpace(lockOffset)
+	newCameraCFrame = newCameraFocus * camRotation
+	return newCameraCFrame, newCameraFocus
+end
+
+function Camera:UpdateXXX(dt)
 	if self.activeCameraController then
 		self.activeCameraController:UpdateMouseBehavior()
 
